8b663976fb8246a85ba2189ec5df60603c207751
Sravanireddy31 <sravanireddygorre31@gmail.com>
2026-03-16 22:16:13 +0530

    Databse creation

---
+++ ai-calling/back-end/server.js
@@ -0,0 +1,20 @@
+ require("dotenv").config()
+
+const fastify = require("fastify")({ logger: true })
+const initDB = require("./src/utils/initDB")
+
+async function start() {
+
+  await initDB()
+
+  fastify.get("/", async () => {
+    return { message: "IVR backend running" }
+  })
+
+  await fastify.listen({ port: 3000 })
+
+  console.log("Server running on port 3000")
+
+}
+
+start()
